b8d28b9d202ccd7c0a0c4f23cf63176a2e1eab83
Kaly <sandra.heraud@ynov.com>
2025-07-21 01:02:20 +0200

    test

--- apps/MyBentoo-Frontend/src/App.tsx
+++ apps/MyBentoo-Frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import * as Sentry from '@sentry/react';
 import './App.css'
 
@@ -10,37 +8,31 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>MyBentoo</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button
-          onClick={() => {
-            try {
-              throw new Error("This is your first error!");
-            } catch (error) {
-              console.error('Erreur capturée localement :', error);
-              Sentry.captureException(error);
-            }
-          }}
-        >
-          Break the world
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <h1>MyBentoo</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button
+            onClick={() => {
+              try {
+                throw new Error("This is your first error!");
+              } catch (error) {
+                console.error('Erreur capturée localement :', error);
+                Sentry.captureException(error);
+              }
+            }}
+          >
+            Break the world
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
