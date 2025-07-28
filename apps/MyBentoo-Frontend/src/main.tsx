7f1e3c2604bc056e04ad7bf7727be4751afa07ee
Sandra Heraud <sandra.heraud@ynov.com>
2025-07-28 15:45:54 +0200

    add environement (#16)

--- apps/MyBentoo-Frontend/src/main.tsx
+++ apps/MyBentoo-Frontend/src/main.tsx
@@ -6,7 +6,7 @@ import * as Sentry from '@sentry/react';
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
-  environment: 'development',
+  environment: import.meta.env.DEPLOY_ENV, 
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
