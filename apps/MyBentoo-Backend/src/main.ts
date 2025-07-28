e53cf8bdf2ec510ff1aa01d0ef4190f8decc7206
Sandra Heraud <sandra.heraud@ynov.com>
2025-07-28 15:17:56 +0200

    Develop (#15)
    * initialisation sentry front only
    
    * test CI/CD
    
    * correc CI/CD
    
    * test ci cd
    
    * test ci cd
    
    * Test
    
    * correc json
    
    * correc
    
    * correction
    
    * ini of replay react with sentry
    
    * correc on deploy + test change front
    
    * add deploy front

--- apps/MyBentoo-Backend/src/main.ts
+++ apps/MyBentoo-Backend/src/main.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -5,15 +8,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   const config = new DocumentBuilder()
     .setTitle('MyBentoo API')
     .setDescription('API for MyBentoo application')
     .setVersion('1.0')
-    .addTag('mybentoo')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/swagger', app, documentFactory);
+
+  await app.listen(process.env.NEST_PORT ?? 3000);
 }
 bootstrap();
